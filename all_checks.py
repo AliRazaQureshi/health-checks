e6167b2f7f12fa98234551fea4bdc3ad3c71988e
AliRazaQureshi <47821624+AliRazaQureshi@users.noreply.github.com>
2020-04-26 02:39:21 +0500

    Initialize check_disk_full()

--- all_checks.py
+++ all_checks.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env/ python3
 
 import os
+import shutil
 import sys
 
 def check_reboot():
 	"""Returns True if computer has a pending reboot."""
 	return os.path.exists("/run/reboot-required")
+
+def check_disk_full(disk, min_absolute, min_percent):
+	pass
 
 def main():
 	if check_reboot():

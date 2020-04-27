15738933a4137a9dd9b8b6b59d496f0ff5f32950
AliRazaQureshi <47821624+AliRazaQureshi@users.noreply.github.com>
2020-04-28 01:31:59 +0500

    .

--- all_checks.py
+++ all_checks.py
@@ -21,7 +21,7 @@ def check_disk_full(disk, min_gb, min_percent):
 
 def main():
 	if check_reboot():
-		print("pending reboot")
+		print("pending reboot.")
 		sys.exit(1)
 	if check_disk_full(disk="/", min_gb=2, min_percent=10):
 		print("Insufficient Space")

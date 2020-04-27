66103ad80964db8a72264494324303c3701aaca2
AliRazaQureshi <47821624+AliRazaQureshi@users.noreply.github.com>
2020-04-28 00:58:46 +0500

    Changing Condition

--- all_checks.py
+++ all_checks.py
@@ -14,7 +14,7 @@ def check_disk_full(disk, min_gb, min_percent):
 	#Calculate percentage of free space
 	percent_free = 100 * du.free / du.total
 	gigabytes_free = du.free / 2**30
-	if percent_free < min_percent or gigabytes_free < min_gb:
+	if min_percent > percent_free or gigabytes_free < min_gb:
 		return True
 	return False
 
